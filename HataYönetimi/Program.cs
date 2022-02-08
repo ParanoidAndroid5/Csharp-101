1e03d0196552ae33c2c7c0d38908b2ccda8c1a07
ParanoidAndroid5 <mislinacetineer@gmail.com>
2022-02-09 00:14:38 +0300

    hata yönetimi

---
+++ HataYönetimi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HataYönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş oldugunuz sayi : " + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.Write("işlem tamamlandi.");
+            }
+
+
+            /*try{ Hataya sebebiyet verme ihtimali olan kod }
+            catch { Hata ile karşılaşıldığında ne yapılacağı buraya yazılır }
+            finally{ Hata olsun olmasın mutlaka yapılmasını istediğimiz işler varsa buraya yazarız }*/
+
+        }
+    }
+}
---
+++ TypeConversion/typeConversion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace patikaCsharp
+{
+    class typeConversion
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion
+            //Bir değişkenin kendinden daha yüksek kapasiteli bir değişkene atanmasıdır. 
+            //Veri kaybı olmaz.
+
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+            int d = a + b + c;
+            Console.WriteLine(d);
+
+            string s = "mislina";
+            char f = 's';
+            object g = s + f;
+            Console.WriteLine(g);
+
+
+
+
+            //Explicit Conversion
+
+            float x = 4.40f;
+            byte y = (byte)x;
+            Console.WriteLine(y);
+
+            // ToString Methodu 
+            int m = 6;
+            string k = m.ToString();
+            Console.WriteLine(k);
+
+            // System.Convert Sınıfı
+            string s1 = "10", s2 = "20";
+            int sayi1, sayi2,toplam;
+
+            sayi1 = Convert.ToInt32(s1);
+            sayi2 = Convert.ToInt32(s2);
+            toplam = sayi2 + sayi1;
+            Console.WriteLine("toplam:" + toplam);
+
+            // Parse Method (stringe dönüstürmek icin)
+            string metin1 = "10";
+            string metin2 = "10.25";
+            int rakam1;
+            double double1;
+
+            rakam1 = Int32.Parse(metin1);
+            double1 = double.Parse(metin2);
+            Console.WriteLine("rakam1:" + rakam1);
+            Console.WriteLine("double1:" + double1);
+
+
+
+
+
+
+
+        }
+    }
+}
