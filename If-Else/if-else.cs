9cdd16d9a032219d250622f2ef311bdcb67e3057
ParanoidAndroid5 <mislinacetineer@gmail.com>
2022-02-24 16:32:26 +0300

    if-else added

---
+++ If-Else/if-else.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time <=18)
+            {
+                Console.WriteLine("iyi günler!");
+            }
+            else
+            {
+                Console.WriteLine("iyi geceler!");
+            }
+
+            string sonuc = time >= 6 && time < 11 ? "günaydin!" : "iyi günleré";
+            Console.WriteLine(sonuc);
+
+
+        }
+    }
+}
