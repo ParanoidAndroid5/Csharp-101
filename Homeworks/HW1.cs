47d0b6006d86a9b2784eb7beb46a9277041778d7
ParanoidAndroid5 <mislinacetineer@gmail.com>
2022-03-08 20:16:12 +0300

    HW-1 added

--- Homeworks/HW1.cs
+++ Homeworks/HW1.cs
@@ -1,9 +1,8 @@
-// 1-Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
-// 2-Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
-// 3-Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
-// 4-Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
 
-//1 
+
+// 1-Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+// Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+//  Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
 
             Console.WriteLine("pozitif bir sayi giriniz.");
             int n = Convert.ToInt32(Console.ReadLine());
@@ -26,7 +25,9 @@
             }
 
 
-//2
+// 2-Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
+// Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+// Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
 
 
             Console.WriteLine("iki pozitif sayi giriniz:");
@@ -48,7 +49,10 @@
 
             }
 
-//3 
+
+// 3-Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n).
+//  Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+//  Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
 
         Console.WriteLine("pozitif bir sayi giriniz.");
                     int n = Convert.ToInt32(Console.ReadLine());
@@ -65,7 +69,10 @@
                         Console.WriteLine(kelime);
                     }
 
-//4
+
+// 4-Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
+// Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+
         Console.WriteLine("Bir cümle giriniz:");
                     string cumle = Console.ReadLine();
                     string[] kelimeler = cumle.Split(' ');
